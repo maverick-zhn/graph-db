4eed62c1af7a6c18c71dabafce59258952919628
ebarsallo <edgardo.barsallo@gmail.com>
2016-02-09 22:06:01 -0500

    start-hbase.sh added.

--- hadoop/conf/hadoop_1.2.1/docker_entrypoint.sh
+++ hadoop/conf/hadoop_1.2.1/docker_entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 service ssh start
+
+# start process hadoop
 /opt/hadoop/bin/start-all.sh
+jps
+
+/opt/hbase/bin/start-hbase.sh
+jps
+
 
 # TODO: open a shell to avoid stop the docker container. Maybe there is a better/elegant solution.
 /bin/bash
